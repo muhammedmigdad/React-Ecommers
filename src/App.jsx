a60e010248e51583bd607ac84ab6db31a47c7ac7
muhammed migdad pt <sinuminhaj45@gmail.com>
2025-02-04 15:50:17 +0530

    Initial commit for react-ecommerce  project

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Orders from './pages/Orders'
+import Placeorder from './pages/Placeorder'
+import Login from './pages/Login'
+import Cart from './pages/Cart'
+import Product from './pages/Product'
+import Navbar from './compontents/Navbar'
+
+
+function App() {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+
+      <Routes>
+        <Route path='/'  element={<Home/>} />
+        <Route path='/collection'  element={<Collection/>} />
+        <Route path='/contact'  element={<Contact/>} />
+        <Route path='/about'  element={<About/>} />
+        <Route path='/orders'  element={<Orders/>} />
+        <Route path='/placeorder'  element={<Placeorder/>} />
+        <Route path='/login'  element={<Login/>} />
+        <Route path='/cart'  element={<Cart/>} />
+        <Route path='/product/:productId'  element={<Product/>} />
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/compontents/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {assets} from '../assets/assets.js'
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div>
+      <div className="flex justify-between items-center py-5 font-medium">
+        <img src={assets.logo} alt="" className="w-36" />
+        <ul className="hidden sm:flex gap5 text-sm text-gray-700">
+          <NavLink to="/" className="flex flex-col items-center gap-1">
+            <p>HOME</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+          </NavLink>
+          <NavLink
+            to="/collection"
+            className="flex flex-col items-center gap-1"
+          >
+            <p>COLLECTION</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+          </NavLink>
+          <NavLink to="/about" className="flex flex-col items-center gap-1">
+            <p>ABOUT</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+          </NavLink>
+          <NavLink to="/contact" className="flex flex-col items-center gap-1">
+            <p>CONTACT</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+          </NavLink>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
